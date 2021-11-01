e80170e7da6b98b5accc2bfd108e795ffb88046a
Derek Roberts <derek.roberts@gmail.com>
2021-11-01 16:27:16 -0700

    refactor: improve dev.sh

--- podman/tinyproxy/dev.sh
+++ podman/tinyproxy/dev.sh
@@ -1,3 +1,2 @@
 docker build . -t tp
-docker run -p 23128:8888 --name=tp -v $(pwd)/conf/:/usr/local/etc/tinyproxy/:z tp
-docker rm -fv tp
+docker run --rm -p 23128:8888 --name=tp -v $(pwd)/conf/:/usr/local/etc/tinyproxy/:z tp

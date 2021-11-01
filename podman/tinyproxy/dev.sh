b3307502f14206c3f6a98b2e2c6fe0b747e286d2
Derek Roberts <derek.roberts@gmail.com>
2021-11-01 15:35:55 -0700

    refactor: flatten conf structure

--- podman/tinyproxy/dev.sh
+++ podman/tinyproxy/dev.sh
@@ -1,4 +1,3 @@
-set -eux
 docker build . -t tp
-docker run -p 23128:8888 --name=tp -v $(pwd)/conf/:/usr/local/etc/:z tp
+docker run -p 23128:8888 --name=tp -v $(pwd)/conf/:/usr/local/etc/tinyproxy/:z tp
 docker rm -fv tp

6834f04bb08e1cad6e29f6671ee70a5c056e3c7d
Ferguson, Brett CSNR:EX <brett@qedsystems.ca>
2021-12-22 11:10:28 -0800

    dev - Docker files moved to containers repo

--- docker/create_image_itcg.sh
+++
@@ -1,2 +0,0 @@
-docker build . --tag bcgov:lb-deployer
-docker run -dit -v ~/.m2/repository:/apps_ux/repo -v ~/git:/home/kstrong/git --name lb-deployer bcgov:lb-deployer
--- docker/files/init_ansible.sh
+++
@@ -1,2 +0,0 @@
-ansible-galaxy collection install git+https://www.github.com/bferguso/nr-ansible,feature/liquibase -p ./collections
-export ANSIBLE_COLLECTIONS_PATH=./collections

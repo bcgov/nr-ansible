18c71f91dd75306065f8a158e1bb0f8b793e0682
Matthew Bystedt <matthew.bystedt@gov.bc.ca>
2021-12-23 10:34:13 -0800

    fix: host metric fields

--- roles/apm_agent/files/conf/filters.lua
+++ roles/apm_agent/files/conf/filters.lua
@@ -73,11 +73,11 @@ end
 
 function add_system_memory_percentage(tag, timestamp, record)
     new_record = record
-    if not(isempty(new_record["system.memory.used.bytes"])) and not(isempty(new_record["system.memory.total"])) then
-        new_record["system.memory.used.pct"] = record["system.memory.used.bytes"] / record["system.memory.total"]
+    if not(isempty(new_record["host.memory.used"])) and not(isempty(new_record["host.memory.total"])) then
+        new_record["host.memory.used_percentage"] = record["host.memory.used"] / record["host.memory.total"]
     end
-    if not(isempty(new_record["system.memory.swap.used.bytes"])) and not(isempty(new_record["system.memory.swap.total"])) then
-        new_record["system.memory.swap.used.pct"] = record["system.memory.swap.used.bytes"] / record["system.memory.swap.total"]
+    if not(isempty(new_record["host.swap.used"])) and not(isempty(new_record["host.swap.total"])) then
+        new_record["host.swap.used_percentage"] = record["system.memory.swap.used.bytes"] / record["host.swap.total"]
     end
     return 2, timestamp, new_record
 end
